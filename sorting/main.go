e4526d8df41cec989a79137d972bec8c7f671bad
threepipes <threepipes@gmail.com>
2023-02-06 22:53:57 +0900

    add quickSort

--- sorting/main.go
+++ sorting/main.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(mergeSort([]int{3, 5, 1, 5, 9, 10, -1}))
-	fmt.Println(mergeSort([]int{3, 5, 1, 5, 9, 10}))
+	fmt.Println(quickSort([]int{3, 5, 1, 5, 9, 10, -1}))
+	fmt.Println(quickSort([]int{3, 5, 1, 5, 9, 10}))
+	fmt.Println(quickSort([]int{1, 2, 3, 4, 5}))
+	fmt.Println(quickSort([]int{5, 4, 3, 2, 1}))
+	fmt.Println(quickSort([]int{1}))
+	fmt.Println(quickSort([]int{1, 1, 1, 1, 1}))
+	fmt.Println(quickSort([]int{}))
 }
 
 func mergeSort(a []int) []int {
@@ -38,4 +43,41 @@ func mergeSort(a []int) []int {
 
 	// return the result
 	return res
+}
+
+func quickSort(a []int) []int {
+	res := make([]int, len(a))
+	copy(res, a)
+	quickSortInner(res, 0, len(res)-1)
+	return res
+}
+
+func quickSortInner(a []int, left, right int) {
+	if right-left < 1 {
+		return
+	}
+	l := left
+	r := right
+	piv := a[(l+r)/2]
+	// fmt.Printf("piv:%v, arr:%v\n", piv, a[left:right])
+	for l < r {
+		for l < r && a[l] < piv {
+			l++
+		}
+		for l < r && a[r] > piv {
+			r--
+		}
+		if l < r {
+			a[l], a[r] = a[r], a[l]
+			l++
+			r--
+		}
+	}
+	if l == r {
+		if a[r] > piv {
+			r--
+		}
+	}
+	quickSortInner(a, left, r)
+	quickSortInner(a, r+1, right)
 }
--- sorting/main_test.go
+++ sorting/main_test.go
@@ -7,10 +7,10 @@ import (
 	"testing"
 )
 
-func test_mergeSort(t *testing.T, arr []int) bool {
+func sortTest(t *testing.T, arr []int, sortFunc func([]int) []int) bool {
 	a1 := make([]int, len(arr))
 	copy(a1, arr)
-	got := mergeSort(a1)
+	got := sortFunc(a1)
 
 	a2 := make([]int, len(arr))
 	copy(a2, arr)
@@ -18,16 +18,24 @@ func test_mergeSort(t *testing.T, arr []int) bool {
 	return reflect.DeepEqual(got, a2)
 }
 
-func Test_mergeSort(t *testing.T) {
+func test_sort(t *testing.T, sortFunc func([]int) []int) {
 	rand.Seed(15)
 	for i := 0; i < 1000; i++ {
-		sz := rand.Intn(100)
+		sz := rand.Intn(1000)
 		arr := make([]int, sz)
 		for j := 0; j < len(arr); j++ {
 			arr[j] = rand.Int()
 		}
-		if !test_mergeSort(t, arr) {
+		if !sortTest(t, arr, sortFunc) {
 			t.Error("Failed. arr=", arr)
 		}
 	}
+}
+
+func Test_mergeSort(t *testing.T) {
+	test_sort(t, mergeSort)
+}
+
+func Test_quickSort(t *testing.T) {
+	test_sort(t, quickSort)
 }
